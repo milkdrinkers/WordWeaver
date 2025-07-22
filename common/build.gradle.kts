8122d514944801395f046fde600add74dad6ddc0
darksaid98 <darksaid9889@gmail.com>
2025-07-22 13:33:22 +0200

    build: fix broken release to maven central option

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -72,7 +72,7 @@ mavenPublishing {
     ))
 
     // Publish to Maven Central
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+    publishToMavenCentral(automaticRelease = true)
 
     // Sign all publications
     signAllPublications()
